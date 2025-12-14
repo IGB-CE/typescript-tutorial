1d3815faffe45df05a7de6a26941ea360fa75f3f
Igli <iglibraho77@gmail.com>
2025-12-14 16:25:39 +0100

    add todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 import './App.css'
 import InputField from './components/InputField'
+import type { Todo } from './model';
 
 // let name: string;
 // // union type for age = number or string
@@ -51,11 +53,26 @@ import InputField from './components/InputField'
 
 // let personName: unknown;
 
+
 const App: React.FC = () => {
+  
+const [todo, setTodo] = useState<string>("");
+const [todos, setTodos] = useState<Todo[]>([]);
+
+const handleAdd = (e: React.FormEvent) => {
+  e.preventDefault();
+  if(todo){
+    setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
+    setTodo("");
+  }
+}
+
+console.log(todos);
+
   return (
     <div className="App">
      <span className="heading">Taskify</span>
-     <InputField />
+     <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   )
 }
